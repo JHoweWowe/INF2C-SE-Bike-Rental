dd10b4f18d92be654d9e1ea739a9e85e3d5ae5bb
rohan-nittur <57628269+rohan-nittur@users.noreply.github.com>
2019-11-24 04:17:55 +0000

    Include getMapOfBikes()

--- src/uk/ac/ed/bikerental/Quote.java
+++ src/uk/ac/ed/bikerental/Quote.java
@@ -1,38 +1,51 @@
 package uk.ac.ed.bikerental;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.*;
 
 public class Quote {
-    
+
     private Provider provider;
     private Collection<Bike> bikes;
     private BigDecimal totalDepositAmount;
     private BigDecimal totalPrice;
-    
+
     public Quote(Provider provider, Collection<Bike> bikes, BigDecimal totalPrice, BigDecimal totalDepositAmount) {
         this.provider = provider;
         this.bikes = bikes;
         this.totalDepositAmount = totalDepositAmount;
         this.totalPrice = totalPrice;
     }
-    
+
     public Provider getProvider() {
         return provider;
     }
-    
+
     public Collection<Bike> getCollectionOfBikes() {
         return bikes;
     }
-    
+
+    public Map<BikeType,Integer> getMapOfBikes(){
+        Map<BikeType,Integer> mapOfBikes = new HashMap<BikeType,Integer>();
+        List<BikeType> bts = new ArrayList<BikeType>();
+        for (Bike b : bikes){
+          bts.add(b.getBikeType())
+        }
+        for (BikeType bt : bts){
+          int num = Collections.frequency(bts,bt);
+          mapOfBikes.put(bt,num);
+        }
+        return mapOfBikes;
+    }
+
     public BigDecimal getTotalDepositAmount() {
         return totalDepositAmount;
     }
-    
+
     public BigDecimal getTotalPrice() {
         return totalPrice;
     }
-    
+
     //Given a Collection of Bikes, this method calculates the total price in the quote
     public BigDecimal calculateTotalPrice(Collection<Bike> bikes) {
         totalPrice = new BigDecimal(0);
@@ -41,12 +54,12 @@ public class Quote {
         }
         return totalPrice;
     }
-    
+
     public BigDecimal calculateDepositAmount(Collection<Bike> bikes) {
         totalDepositAmount = new BigDecimal(0);
         totalDepositAmount = provider.getDepositRate().multiply(calculateTotalPrice(bikes));
         return totalDepositAmount;
-        
+
     }
 
 }
